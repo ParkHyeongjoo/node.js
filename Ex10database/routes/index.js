25ee198b5df313681606f3cc9919ee6ad2fbade7
JParkBro <hyeongjoopark0324@gmail.com>
2022-11-25 16:23:05 +0900

    20221125 node.js , database 연결

--- Ex10database/routes/index.js
+++ Ex10database/routes/index.js
@@ -33,7 +33,52 @@ router.post("/insert", (req, res) => {
       // 성공
       res.redirect("/select");
     }
-    res.send("INSERT 완료");
+  });
+});
+
+router.get("/select/:id", (req, res) => {
+  let id = req.params.id;
+  console.log("paramID : " + id);
+
+  let sql = "SELECT * FROM member WHERE id=?";
+
+  conn.query(sql, [id], function (err, result, fields) {
+    console.log(result);
+    if (err) {
+      console.error("SELECT 실행 실패! : " + err);
+    } else {
+      res.json({ listOne: result });
+    }
+  });
+});
+
+router.get("/delete/:id", (req, res) => {
+  let id = req.params.id;
+
+  let sql = "DELETE FROM member WHERE id=?";
+
+  conn.query(sql, [id], function (err, result, fields) {
+    console.log(result);
+    if (err) {
+      console.error("DELETE 실행 실패 ! : " + err);
+    } else {
+      res.redirect("/select");
+    }
+  });
+});
+
+router.post("/update", (req, res) => {
+  let { id, pw, nick } = req.body;
+
+  let sql = "UPDATE member SET pw=? , nick=? WHERE id=?";
+
+  conn.query(sql, [pw, nick, id], function (err, result, fields) {
+    console.log(result);
+    if (err) {
+      console.error("UPDATE 실행 실패 ! : " + err);
+    } else {
+      res.redirect("/select");
+    }
   });
 });
 
